4dc99d3d6adc9f057ed40b672a83e0abab495708
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-01-20 13:09:07 +0100

    Squashed 'plugin/emitter/' content from commit f159aab
    git-subtree-dir: plugin/emitter
    git-subtree-split: f159aabf092593cf6b433c48f63ea68f78214997

---
+++ build.bat
@@ -0,0 +1,20 @@
+go test ./...
+%GOPATH%/bin/overalls -project=github.com\emitter-io\emitter
+%GOPATH%/bin/goveralls -coverprofile=overalls.coverprofile -service=appveyor-ci -repotoken=%COVERALLS_TOKEN%
+
+call :build linux arm  
+call :build linux amd64  
+call :build linux 386  
+call :build darwin amd64  
+call :build darwin 386   
+call :build windows 386 .exe
+call :build windows amd64 .exe
+goto :end
+
+:build
+	set GOOS=%1
+	set GOARCH=%2
+	go tool dist install pkg/runtime
+	go install -a std
+	go build -o build/emitter-%1-%2%3 -i .
+:end
---
+++ network/http/http_test.go
@@ -0,0 +1,91 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/emitter-io/emitter/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+type testHandler struct{}
+
+type testObject struct {
+	Field string `json:"field"`
+}
+
+func TestNewClient(t *testing.T) {
+	tests := []struct {
+		url string
+		ok  bool
+	}{
+		{url: "http://google.com/123", ok: true},
+		{url: "google.com/123", ok: false},
+		{url: "235235", ok: false},
+		{url: "::", ok: false},
+	}
+
+	for _, tc := range tests {
+		c, err := NewClient(tc.url, time.Second)
+		if tc.ok {
+			assert.NotNil(t, c)
+			assert.NoError(t, err)
+		} else {
+			assert.Nil(t, c)
+		}
+	}
+}
+
+func (h *testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var response []byte
+	if r.Header.Get("Content-Type") == "application/binary" {
+		w.Header().Set("Content-Type", "application/binary")
+		response, _ = utils.Encode(&testObject{
+			Field: "response",
+		})
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		response, _ = json.Marshal(&testObject{
+			Field: "response",
+		})
+	}
+
+	w.Write(response)
+}
+
+func TestPostGet(t *testing.T) {
+	s := httptest.NewServer(new(testHandler))
+	defer s.Close()
+	body := testObject{Field: "hello"}
+	expect := &testObject{Field: "response"}
+
+	jsonBody, _ := json.Marshal(body)
+
+	// Reuse the client
+	c, err := NewClient(s.URL, time.Second, NewHeader("Authorization", "123"))
+	assert.NoError(t, err)
+
+	{
+		output := new(testObject)
+		_, err := c.Get(s.URL, output)
+		assert.NoError(t, err)
+		assert.EqualValues(t, expect, output)
+	}
+
+	{
+		body, err := c.Get(s.URL, nil)
+		assert.NoError(t, err)
+		assert.NotNil(t, body)
+	}
+
+	{
+		output := new(testObject)
+		_, err := c.Post(s.URL, jsonBody, output)
+		assert.NoError(t, err)
+		assert.EqualValues(t, expect, output)
+	}
+
+}
